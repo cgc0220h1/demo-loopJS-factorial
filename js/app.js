a00b6fe148fe098f4314ee0d8b24e502a0eedf59
vanduc2514 <vanduc2514@gmail.com>
2020-03-02 20:13:10 +0700

    Sửa lại thuật toán

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 function startCal() {
     let num = +prompt("Nhập vào số cần tính giai thừa");
     document.getElementById('num-input').innerHTML = "Số cần tính giai thừa là: " + num;
+    document.getElementById('result').innerHTML = "Kết quả là: " + factorialCal(num);
+}
+
+function factorialCal(number) {
     let result = 1;
-    while (num > 0) {
-        result *=  num;
-        num--;
+    while (number > 0) {
+        result *= number;
+        number--;
     }
-    document.getElementById('result').innerHTML = "Kết quả là: " + result;
+    return result;
 }
